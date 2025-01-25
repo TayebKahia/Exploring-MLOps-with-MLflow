4c90552fd4f59677d5e97834749844da4c42cefd
Tayeb Kahia <t.kahia@esi-sba.dz>
2025-01-25 19:30:53 +0100

    add model registry

--- register_model.py
+++ register_model.py
@@ -1,8 +1,23 @@
-model_name = "XGB-Smote"
-run_id = "64c9651ae0de42ba898917002ffb2ccb/model"
-model_uri = f"runs:/{run_id}/model_name"
+import mlflow
 
-print("MLflow is running")
+run_id = "64c9651ae0de42ba898917002ffb2ccb"
+# model_name = "CaliforniaHousingModel"
+# model_uri = f"runs:/{run_id}/model_name"
 
-with mlflow.start_run(run_id=run_id):
-    mlflow.register_model(model_uri=model_uri, name=model_name)
+# with mlflow.start_run(run_id=run_id):
+#     mlflow.register_model(model_uri=model_uri, name=model_name)
+
+
+from mlflow.tracking import MlflowClient
+
+model_uri = f"runs:/{run_id}/model"
+model_name = "CaliforniaHousingModel"
+
+client = MlflowClient()
+# Transition the model to the "Staging" stage
+client.transition_model_version_stage(
+    name=model_name,
+    version=2,  # Replace with your registered model's version
+    stage="Staging",
+)
+print(f"Model {model_name} version 2 transitioned to 'Staging'.")
